baad73d43d8cd35aff5a7001ea4c3c8bb635f3cb
Chad <chad.delorean@gmail.com>
2014-03-21 19:49:21 -0600

    serviceList and eventsignup added

--- js/scripts.js
+++ js/scripts.js
@@ -14,4 +14,28 @@ YSA11Service.controller('homePageCtrl', function($scope, $http) {
 	$http.get('clipBoard.php').success(function (data) {
 		$scope.clipBoard = data;
 	});
+	
+	$http.get('serviceList.php').success(function (data) {
+		$scope.events = data;
+	});
+	
+	$scope.goToEventSignup = function(id) {
+		sessionStorage.setItem('id', id);
+		location.href= "eventsignup.html";
+	};
+});
+
+YSA11Service.controller('serviceListingCtrl', function($scope, $http) {
+	$http.get('serviceList.php').success(function (data) {
+		$scope.events = data;
+	});
+	
+	$scope.goToEventSignup = function(id) {
+		sessionStorage.setItem('id', id);
+		location.href= "eventsignup.html";
+	};
+});
+
+YSA11Service.controller('eventSignupCtrl', function($scope, $http) {
+	 $scope.id = sessionStorage.getItem('id');
 });
--- nav.php
+++ nav.php
@@ -1,10 +1,10 @@
 <header id="header">
 	<h1><a href="#" id="logo">Service Opportunities</a></h1>
 	<nav id="nav">
-		<a href="index.php">Homepage</a>
+		<a href="index.html">Homepage</a>
 		<a href="serviceList.html">Service Projects</a>
-		<a href="bulletin.php">Bulletin</a>
-		<a href="requests.php">Service Requests</a>
-		<a href="about.php">About</a>
+		<a href="bulletin.html">Bulletin</a>
+		<a href="requests.html">Service Requests</a>
+		<a href="about.html">About</a>
 	</nav>
 </header>
